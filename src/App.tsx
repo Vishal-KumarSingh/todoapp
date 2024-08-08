69899b37557338f047a272e7fe5aa6deea27060c
vishal <vishalkrsinghmsk@gmail.com>
2024-08-09 01:16:37 +0530

    intiial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import {Dashboard} from './pages/Dashboard'
+import './App.css'
+import NavBar from './sections/NavBar'
+
+function App() {
+
+  
+  return (
+    <BrowserRouter>
+    
+    <nav> 
+      <div className='logo text-white p-3'>
+
+      To Do 
+      </div>
+     
+
+      
+    </nav>
+    <div className='page'>
+    <Routes>
+
+         <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+    </div>
+    <NavBar />
+    </BrowserRouter>
+  )
+}
+
+export default App
